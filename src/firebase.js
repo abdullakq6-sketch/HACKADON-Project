f6fc43d0e7815ce188ed8a642df084e5534cf5af
Abdullah <abdullakq6@gmail.com>
2026-07-15 10:28:24 +0500

    Secure environment variables and update gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyA2GsapelFGjADEvTXWZF16j0nXl7YpGfw",
+  authDomain: "maintainiq-ai.firebaseapp.com",
+  projectId: "maintainiq-ai",
+  storageBucket: "maintainiq-ai.firebasestorage.app",
+  messagingSenderId: "2722194127",
+  appId: "1:2722194127:web:935e21dd4823b32a61cdba",
+  measurementId: "G-B1KPJVNBFG"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const auth = getAuth(app); 
